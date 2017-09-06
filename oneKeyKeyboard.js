aeab4df47e90370ff0d4a833c78675880ace0dc0
teritry <teritry2@gmail.com>
2017-09-06 18:33:02 +0200

    Added button function and function for inserting text into the text field.

--- oneKeyKeyboard.js
+++ oneKeyKeyboard.js
@@ -2,4 +2,24 @@
  * Created by Teritry on 06.09.2017.
  */
 
+var timePressed = 0;
+var text = "a";
+window.onkeypress = function(e) {
+    if(e.which == 13) {
+            if(!timePressed){
+                timePressed = (new Date()).getTime();
+            }
+    }
+}
 
+
+window.onkeyup = function(e) {
+    if(e.which == 13) {
+        var timePressedDown = (new Date()).getTime() - timePressed;
+        if (true){
+            $('#oneButtonEditor').val($('#oneButtonEditor').val() + text);
+            console.log(timePressedDown);
+            timePressed = 0;
+        }
+    }
+}
